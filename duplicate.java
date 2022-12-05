850cf21cfc319ccb8a680008675c690ed9a3309c
Bhagyashreegodase <118460084+Bhagyashreegodase@users.noreply.github.com>
2022-12-05 12:31:30 +0530

    Add files via upload

---
+++ duplicate.java
@@ -0,0 +1,26 @@
+//author: bhagyashree
+package com. question3;
+ import java.util Scanner {
+     public static void main(string[]args)
+{
+ Scanner sc=newSacnner(syestem.in);
+  string sr=("Hi my name Is bhagyashree And I love To proggraming language");
+ system .out.println("enter the string");
+   string sr2=sc.nextLine();
+   system .out.println("after the removing teh dupilicate character"); 
+     for(int i=0; i<sr.lenght();i++){
+                boolean b=false;
+                 for(int  j=0;j<sr2.length();j++){
+     if(charcter.toLowerCase(sr.char At(i)==Charcter toLowerCase(sr2.charAt(j))){
+b=true; 
+     break;
+       }
+ }
+ if(b==false)
+     system.out.print(sr.charAt(i));
+              }
+                     }
+                               }
+           
+
+
