721e3c4534c48f4bf44accc59f21deb5e3721e3f
Bhagyashreegodase <118460084+Bhagyashreegodase@users.noreply.github.com>
2022-12-05 12:22:14 +0530

    Add files via upload

---
+++ find sequences.java
@@ -0,0 +1,17 @@
+//aurhor:bhagyshree
+importjava.util.Scanner;
+public class test {
+  public static void main (string[]args){
+      system .out.println(validate("java_Exercises"));
+      system.out.println(validate("java_Exercises"));
+      system.out.println(validate("java_Exercises"));
+      system.out.println(validate("java_Exercises"));
+}
+  public Stastic String validate(String text) {
+        if (text.matches("^[a-z+_[a-z+$"))
+        return "Found a match !";
+       else  
+            return" Not matched !" ;
+    }
+       }
+      
